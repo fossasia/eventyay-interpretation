0e773686fb27ad078bb2c31d2051b871b48d5387
Arnav Angarkar <arnav.angarkar20@gmail.com>
2026-08-09 02:22:47 +0530

    feat: delete booths that are removed from languages

--- interpretation/backends/voxbento.py
+++ interpretation/backends/voxbento.py
@@ -72,6 +72,16 @@ class VoxbentoBackend(InterpreterBackend):
             except requests.RequestException:
                 pass
 
+        # Find languages that were removed and delete them from VoxBento
+        removed_langs = [l for l in booths.keys() if l not in interpretation.target_languages]
+        for lang in removed_langs:
+            delete_url = f"{base_url.rstrip('/')}/api/events/{event.slug}/rooms/{interpretation.room_id}/booths/{lang}"
+            try:
+                requests.delete(delete_url, headers=headers, timeout=5.0)
+            except requests.RequestException:
+                pass
+            booths.pop(lang, None)
+
         config["booths"] = booths
         interpretation.backend_config = config
         interpretation.save(update_fields=["backend_config"])
